9bfb21450130e0052f4838230e204112c1319ecf
ffelipesenac <125416436+ffelipesenac@users.noreply.github.com>
2023-03-21 21:26:51 -0300

    Add files via upload

---
+++ cariocatech/home.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Carioca Tech</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/slide.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/slide.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/slide.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+
+  <div class="produtos">
+
+    <div class="card" style="width: 18rem;">
+  <img src="img/COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB(700x700).jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <span class="preco">R$ 1.000,00</span>
+    <span>À vista no PIX</span>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+  <img src="img/COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB(700x700).jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <span class="preco">R$ 1.000,00</span>
+    <span>À vista no PIX</span>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="img/COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB(700x700).jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <span class="preco">R$ 1.000,00</span>
+    <span>À vista no PIX</span>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="img/COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB(700x700).jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">COMPUTADOR MANCER GAMER LORRIDE, AMD RYZEN 3 3200G, 16GB DDR4, SSD 240GB</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <span class="preco">R$ 1.000,00</span>
+    <span>À vista no PIX</span>
+    <a href="#" class="btn btn-primary">Comprar</a>
+  </div>
+</div>
+
+</div>
+
+  <div class="banner">
+    <img src="img/banner.png" alt="">
+  </div>
+
+
+  <div id="categorias">
+    <div class="container text-center">
+    <div class="row categorias">
+     <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      </div>
+    </div>
+
+
+    <div class="container text-center">
+    <div class="row categorias">
+     <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      </div>
+    </div>
+
+
+    <div class="container text-center">
+    <div class="row categorias">
+     <div class="col">
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      <div class="col">
+          <img src="img/processador i5 (700x700).jpg" alt="">
+      </div>
+      </div>
+    </div>
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
